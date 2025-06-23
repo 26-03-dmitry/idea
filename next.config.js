3f11e8cbb4e0b87d1ee19da98ff1e4d4e08c3142
26-03-dmitry <your-email@example.com>
2025-06-23 14:05:21 +0400

    Исправлена конфигурация Firebase Hosting для Next.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,20 @@
-    /** @type {import('next').NextConfig} */
-    const nextConfig = {
-      output: 'export',
-      images: {
-        unoptimized: true,
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'images.unsplash.com',
-          },
-          {
-            protocol: 'https',
-            hostname: 'example.com',
-          },
-        ],
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
       },
-    };
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+      },
+    ],
+  },
+};
 
-    module.exports = nextConfig;
+module.exports = nextConfig;
