edc2db525c0795ffdaac933ea54313610c7ecda6
26-03-dmitry <your-email@example.com>
2025-06-23 05:31:33 +0400

    fix(build): create i18n.config and fix imports

--- app/[lang]/post-ad/page.tsx
+++ app/[lang]/post-ad/page.tsx
@@ -1,13 +1,16 @@
 import { getDictionary } from '@/lib/dictionary'
-import { Locale } from '@/i18n.config'
+import { type Locale, i18n } from '@/i18n.config'
 import PostAdPageClient from '@/components/PostAdPageClient'
-import { i18n } from '@/i18n.config'
 
 export async function generateStaticParams() {
-  return i18n.locales.map((locale) => ({ lang: locale }))
+  return i18n.locales.map(locale => ({ lang: locale }))
 }
 
-export default async function PostAdPage({ params: { lang } }: { params: { lang: Locale } }) {
+export default async function PostAdPage({
+  params: { lang },
+}: {
+  params: { lang: Locale }
+}) {
   const dictionary = await getDictionary(lang)
   return <PostAdPageClient dictionary={dictionary} lang={lang} />
 } 
